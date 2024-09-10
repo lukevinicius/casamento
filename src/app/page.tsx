667eb45dbdd38189b496beaf84f87a02639758df
lukevinicius <lucasviniciusaalves@gmail.com>
2024-09-09 22:06:21 -0300

    update: list products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,12 @@
 import { Products } from '@/components/home/products'
-import { ChevronsDown } from 'lucide-react'
-import Image from 'next/image'
-import conviteImg from '@/assets/convite.png'
-import { ConfirmButton } from '@/components/buttons/confirm-button'
-
 import { CopyPIX } from '@/components/buttons/copy-pix'
 
 export default function Home() {
   return (
     <div className="h-full bg-white">
-      <div className="mx-auto md:max-w-96">
-        <div className="h-full">
-          <Image src={conviteImg} alt="Convite" />
-          <div className="mx-4 flex h-full justify-center rounded-t-xl bg-emerald-700 p-4 text-center text-zinc-50">
-            <div className="space-y-4">
-              <p className="text-lg">Gostaria de confirmar sua presença?</p>
-              <ChevronsDown className="mx-auto h-12 w-12 animate-bounce" />
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="mx-auto h-full md:max-w-96">
-        <div className="mx-4 flex h-full justify-center bg-emerald-700 px-4 pb-4 text-center text-zinc-50">
+        <div className="mx-4 mt-4 flex h-full justify-center rounded-t-xl bg-emerald-700 p-4 pb-4 text-center text-zinc-50">
           <div className="space-y-6">
-            <ConfirmButton />
             <p>
               Sua presença é o nosso maior presente, pois a maior alegria da
               nossa jornada tem sido compartilhar cada momento com as pessoas
